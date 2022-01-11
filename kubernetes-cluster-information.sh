8500bb86e14bc32ed6b513f5baa39714a69059f4
marcus-kielman <marcus.x.kielman@gmail.com>
2022-01-11 17:13:45 -0500

    SBA Changes: Modified kubernetes.yml to run app in kubernetes, and Created Shell Script to Write Status of Nodes

---
+++ kubernetes-cluster-information.sh
@@ -0,0 +1,20 @@
+!#/bin/bash
+echo "Kubernetes Cluster Information\n\n"
+
+echo "    Cluster Info"
+kubectl cluster-info
+
+echo "    Kubernetes Deployments"
+kubectl get deployments web-sba
+
+echo "    Kubernetes Services"
+kubectl get service web-sba
+
+echo "    Kubernetes Pods"
+kubectl get pods -l app=web
+
+echo "    Kubernetes Nodes"
+kubectl get nodes
+
+echo "    Kubernetes IP Addresses"
+kubectl get nodes -o wide
