36539b419e001213a4c839522c8baa00d21f3b43
hakanco <colak.hakan@icloud.com>
2022-01-11 15:57:27 -0500

    updated

--- kubernetes-cluster-information.sh
+++ kubernetes-cluster-information.sh
@@ -3,4 +3,4 @@
 kubectl describe svc > kubernetes-cluster-information-output
 kubectl describe pod >> kubernetes-cluster-information-output
 kubectl describe nodes >> kubernetes-cluster-information-output
-kubectl describe deployments 
+kubectl describe deployments >> kubernetes-cluster-information-output
